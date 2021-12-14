ec840479ad216d15eba89c7ae7583cbba33f7740
Roed <Geofery10@hotmail.com>
2021-12-14 08:43:50 +0100

    Update FrontController.java

--- src/main/java/now/planit/Controllers/FrontController.java
+++ src/main/java/now/planit/Controllers/FrontController.java
@@ -3,6 +3,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.http.HttpSession;
 
 @org.springframework.stereotype.Controller
+@Controller
 public class FrontController {
 
 
