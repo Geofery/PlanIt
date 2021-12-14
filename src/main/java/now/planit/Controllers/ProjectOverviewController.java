f54e0ee62d703972d5a2ee0904a30db7473285a1
Roed <Geofery10@hotmail.com>
2021-12-14 13:52:08 +0100

    update dependencies

--- src/main/java/now/planit/Controllers/ProjectOverviewController.java
+++ src/main/java/now/planit/Controllers/ProjectOverviewController.java
@@ -2,24 +2,15 @@ package now.planit.Controllers;
 
 import now.planit.Data.Repo.FacadeMySQL;
 import now.planit.Data.Repo.MapperDB;
-import now.planit.Data.Repo.ProjectRepo;
 import now.planit.Data.Repo.UsersRepo;
-import now.planit.Domain.Models.Project;
-import now.planit.Domain.Models.Subtask;
-import now.planit.Domain.Models.Task;
 import now.planit.Domain.Models.User;
-import now.planit.Domain.Services.ProjectService;
-import now.planit.Domain.Services.SubtaskService;
-import now.planit.Domain.Services.TaskService;
 import now.planit.Domain.Services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
 
 /**
  * @author roed
